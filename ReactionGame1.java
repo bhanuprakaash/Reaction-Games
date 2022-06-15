fb4c4ec1af3e7cb53fb7c4c281a788d7ae09b308
Bhanu Prakash Sai <67861031+bhanuprakaash@users.noreply.github.com>
2022-06-15 11:55:55 +0530

    Create ReactionGame1.java

---
+++ ReactionGame1.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class ReactionGameBhanuPrakash {
+
+  public static final void main(String args[]) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Be ready!");
+    sleepFor(5);
+    System.out.println("Quick, hit the enter key!");
+    double StartTime = getNowSeconds();
+    String input = scanner.nextLine();
+    double EndTime = getNowSeconds();
+    System.out.println("You took " + (EndTime - StartTime) + " seconds");
+    System.out.println("End");
+  }
+
+  private static void sleepFor(int seconds) {
+    try {
+      Thread.sleep(seconds * 1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static double getNowSeconds() {
+    return new Date().getTime() / 1000.0;
+  }
+}
